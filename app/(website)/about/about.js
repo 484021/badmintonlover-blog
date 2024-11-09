90abd2d252e037cbe5cd8eb6151caf9f2863c9c3
484021 <bhoopalsanthosh@gmail.com>
2024-11-09 17:43:23 -0500

    feat: about page

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -6,20 +6,20 @@ import Link from "next/link";
 export default function About({ authors, settings }) {
   return (
     <Container>
-      <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
+      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
         About
       </h1>
       <div className="text-center">
-        <p className="text-lg">We are a small passionate team.</p>
+        <p className="text-lg">We are doing this out of the love for Badminton. We want to more people in the world to play and enjoy Badminton. Our goal is to promote Badminton to the world.</p>
       </div>
 
-      <div className="grid grid-cols-3 gap-5 mt-6 mb-16 md:mt-16 md:mb-32 md:gap-16">
+      <div className=" mb-16 mt-6 gap-5 md:mb-32 md:mt-16 md:gap-16">
         {authors.slice(0, 3).map(author => {
           const imageProps = urlForImage(author?.image) || null;
           return (
             <div
               key={author._id}
-              className="relative overflow-hidden rounded-md aspect-square odd:translate-y-10 odd:md:translate-y-16">
+              className="relative aspect-square overflow-hidden rounded-md odd:translate-y-10 odd:md:translate-y-16">
               <Link href={`/author/${author.slug}`}>
                 <Image
                   src={imageProps.src}
@@ -34,18 +34,22 @@ export default function About({ authors, settings }) {
         })}
       </div>
 
-      <div className="mx-auto prose text-center dark:prose-invert mt-14">
+      <div className="prose mx-auto mt-14 text-center dark:prose-invert">
         <p>
-          We provide real-time connectivity to enable software
-          providers and financial institutions to build integrated
-          products for their small business customers.
+          I’m Santhosh Bhoopal, a lifelong badminton enthusiast and
+          the creator of Badminton Lover. I’ve spent years both
+          playing and studying the sport, and I’m excited to share
+          practical tips, tutorials, and insights to help players of
+          all levels improve their game. When I’m not on the court or
+          writing about badminton, you’ll find me working on tech
+          projects and diving into web development.
         </p>
-        <p>
+        {/* <p>
           Our API infrastructure is leveraged by clients ranging from
           lenders to corporate card providers and business forecasting
           tools, with use cases including automatic reconciliation,
           business dashboarding, and loan decisioning.
-        </p>
+        </p> */}
         <p>
           <Link href="/contact">Get in touch</Link>
         </p>
