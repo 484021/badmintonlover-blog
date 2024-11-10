c234ccfd7494e154590b6f2587f2c0756364eaf4
484021 <bhoopalsanthosh@gmail.com>
2024-11-09 21:00:24 -0500

    feat: new post page

--- app/(website)/post/[slug]/page.js
+++ app/(website)/post/[slug]/page.js
@@ -1,6 +1,31 @@
+// import PostPage from "./default";
+
+// import { getAllPostsSlugs, getPostBySlug } from "@/lib/sanity/client";
+
+// export async function generateStaticParams() {
+//   return await getAllPostsSlugs();
+// }
+
+// export async function generateMetadata({ params }) {
+//   const post = await getPostBySlug(params.slug);
+//   return { title: post.title };
+// }
+
+// export default async function PostDefault({ params }) {
+//   const post = await getPostBySlug(params.slug);
+//   return <PostPage post={post} />;
+// }
+
+
+// // export const revalidate = 60;
+
 import PostPage from "./default";
 
-import { getAllPostsSlugs, getPostBySlug } from "@/lib/sanity/client";
+import {
+  getAllPostsSlugs,
+  getPostBySlug,
+  getTopCategories,
+} from "@/lib/sanity/client";
 
 export async function generateStaticParams() {
   return await getAllPostsSlugs();
@@ -13,7 +38,8 @@ export async function generateMetadata({ params }) {
 
 export default async function PostDefault({ params }) {
   const post = await getPostBySlug(params.slug);
-  return <PostPage post={post} />;
+  const categories = await getTopCategories();
+  return <PostPage post={post} categories={categories} />;
 }
 
 // export const revalidate = 60;
